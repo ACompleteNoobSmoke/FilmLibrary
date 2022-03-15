691a87505e5c1204640f6b038551343716b2afbb
ACompleteNoobSmoke <OmoruyiOmofonmwan@yahoo.com>
2022-03-15 05:02:46 -0500

    Started working on the styling of the project

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,19 @@
 const express = require('express');
+const Film = require('../model/film');
 const router = express.Router();
 
 
 //Route to the index page
-router.get('/', (req, res) => {
-    res.render('index');
+router.get('/', async (req, res) => {
+    let films;
+    try{
+        films = await Film.find().limit(5).exec();
+    }catch{
+        films = [];
+    }
+
+    res.render('index', {films:films})
+
 })
 
 module.exports = router;
